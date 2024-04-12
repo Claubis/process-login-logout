53674eeb12477d08c20f128c7da824b03dbc5d33
Claudio Silva Bispo <110735259+Claubis@users.noreply.github.com>
2024-04-11 22:38:28 -0300

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,72 @@
+
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+const LoginPage : React.FC = () => {
+
+    const [username, setUserName] =  useState("");
+    const [senha, setSenha] =  useState("");
+    const router = useRouter();
+    const [error, setError] = useState("");
+
+    const handleLogin = () => {
+        if(username === 'admin' && senha === 'admin' ){
+
+            // Primeiro eu crio um estado que deixa a informação congelada, sem renderizar automáticamente.
+            //event?.defaultPrevented();
+
+            // No  localStore os dados ficam infinitos, já no sectionStorage não.
+            localStorage.setItem('isLoggedIn','true')
+
+            // Agora eu tenho que redirecionar
+            router.push('/dashboard');
+            
+        }else{
+            setError("Usuário não encontrado!");
+        } 
+    }
+
+    return(
+
+        <main>
+
+            <form action="">
+
+                <label htmlFor="name">Usuário
+                    
+                    <input 
+                    type="text" 
+                    placeholder="Username"  
+                    value={ username }
+                    onChange={
+                        (event) => setUserName(event.target.value)} />
+
+                </label>
+                
+                <br />
+
+                <label htmlFor="senha">Digite sua senha
+                    
+                    <input 
+                    type="text" 
+                    name="senha" 
+                    id="senha" 
+                    placeholder="Digite sua senha" 
+                    value={ senha} 
+                    onChange={ (event) => setSenha (event.target.value) }/>
+
+                </label>
+
+                <div>
+                    <button type="button" onClick={ handleLogin }>Login</button>
+                </div>
+
+                {error && <span> { error }  </span>}
+
+            </form>
+
+        </main>
+    );
+}
+
+export default LoginPage;
